7cbec4d7750f01119295c3d961c8591f5b9c5741
cybr-wisp <marierubybeaufort@gmail.com>
2026-09-01 13:00:19 -0400

    feat(api): add spring-kafka, jackson deps and demo/prod config

--- vanguard-api/src/main/java/com/vanguard/api/VanguardApplication.java
+++ vanguard-api/src/main/java/com/vanguard/api/VanguardApplication.java
@@ -2,8 +2,10 @@ package com.vanguard.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VanguardApplication {
     public static void main(String[] args) {
         SpringApplication.run(VanguardApplication.class, args);
